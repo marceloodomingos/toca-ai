cb991909f48687b95d124ea054d258f6a462cb3c
Marcelo Augusto Domingos <60621494+gelzinn@users.noreply.github.com>
2022-08-25 11:56:11 -0300

    search engine

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import axios from "axios";
 import Header from "~/components/Header";
 
 import { Features, MostPopularSongs, Presentation } from "~/styles/pages/home";
